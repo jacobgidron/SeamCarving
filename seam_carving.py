201332cabe95ba0e06433c743036fb2134965368
ido-co <its.ido@gmail.com>
2022-03-27 19:34:20 +0300

    initial utils

--- seam_carving.py
+++ seam_carving.py
@@ -1,6 +1,15 @@
-from typing import Dict, Any
+from typing import Dict, Any, Tuple
+
+import numpy as np
+
+import utils
 
 NDArray = Any
+
+
+def find_vrt_seams(image: NDArray, k: int) -> NDArray:
+    pd = np.zeros(image.shape[0], k)
+
 
 
 def resize(image: NDArray, out_height: int, out_width: int, forward_implementation: bool) -> Dict[str, NDArray]:
@@ -14,7 +23,21 @@ def resize(image: NDArray, out_height: int, out_width: int, forward_implementati
                                     the basic implementation is used.
     :return: A dictionary with three elements, {'resized' : img1, 'vertical_seams' : img2 ,'horizontal_seams' : img3},
             where img1 is the resized image and img2/img3 are the visualization images
-            (where the chosen seams are colored red and black for vertical and horizontal seams, respecitvely).
+            (where the chosen seams are colored red and black for vertical and horizontal seams, respectively).
     """
-    raise NotImplementedError('You need to implement this!')
+    o_height, o_width, _ = image.shape
+    gray = utils.to_grayscale(image)
+    idx_mat = np.arange(o_height * o_width).reshape(o_height, o_width)
+
+    def o_idx(idx: int) -> Tuple[int, int]:
+        return idx // o_width, idx % o_width
+
+    vrt_k, hrz_k = abs(out_width - o_width), abs(out_height - o_height)
+    vrt_seams = []
+    hrz_seams = []
+
+
+
+
+
     # TODO: return { 'resized' : img1, 'vertical_seams' : img2 ,'horizontal_seams' : img3}
