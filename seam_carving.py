4197e3855b7d6d1b4bcb97c37d52069aa26f358a
jacob <jacobgidron@mail.tau.ac.il>
2022-03-29 16:50:18 +0300

    first working version of resize

--- seam_carving.py
+++ seam_carving.py
@@ -10,7 +10,7 @@ NDArray = Any
 def delete_seam(image: NDArray, idxs: NDArray, seam: NDArray) -> NDArray:
     h, w = image.shape[0], image.shape[1]
     mask = np.ones((h, w), dtype=bool)
-    mask[range(h), seam] = False
+    mask[range(h), seam+1] = False
     return image[mask].reshape(h, w - 1), idxs[mask].reshape(h, w - 1)
 
 
@@ -45,31 +45,47 @@ def color_seams(image: NDArray, seams: NDArray, col: str) -> NDArray:
     image[rows, seams] = color
 
 
-def find_vrt_seams(image: NDArray, k: int) -> NDArray:
+def find_vrt_seams(image: NDArray, k: int, idx_mat: NDArray) -> NDArray:
     """
     :param image:
     :param k:
     :return:
     """
-    # pad the image 1 column each side
-    # todo
 
-    seams = np.zeros((image.shape[0], k))
-    c = np.zeros((3, image.shape[1]))
-    m = np.zeros_like(image)
-    keys = np.zeros_like(image)
+    seams = np.zeros((image.shape[0], k), dtype=int)
+    keys = np.zeros_like(image, dtype=int)
+    idx = np.arange(keys.shape[1], dtype=int)
     energy = utils.get_gradients(image)
+    c = np.zeros((3, image.shape[1]))
+
+    # pad the image 1 column each side
+    pad = np.zeros((image.shape[0], 1)) + np.nan
+    image = np.hstack((pad, image, pad))
+    m = np.zeros(image.shape[1])
+    m[[0, -1]] = np.nan
     for i in range(k):
-        for row in range(1, m.shape[0]):
-            # calculate the cost of the last row
+        # first row is only the energy
+        m[1:-1] = energy[0, :]
+        for row in range(1, keys.shape[0]):
+            # calculate the cost of the last row for each option
             c[:, :] = np.absolute(image[row, :-2] - image[row, 2:])
-            c[0, :] += np.absolute(image[row - 1, 1:-1] - image[row, :-2]) + m[row - 1, :-2]
-            c[1, :] += m[row - 1, 1:-1]
-            c[2, :] += np.absolute(image[row - 1, 1:-1] - image[row, 2:]) + m[row - 1, 2:]
+            np.nan_to_num(c, nan=255, copy=False)
+            c[0, :] += np.absolute(image[row - 1, 1:-1] - image[row, :-2]) + m[:-2]
+            c[1, :] += m[1:-1]
+            c[2, :] += np.absolute(image[row - 1, 1:-1] - image[row, 2:]) + m[2:]
             # add a pixel energy to the cost
-            m[row, :] = energy[row, :] + np.min(c, axis=0)
-            # save the indexes for later
-            keys[row, :] = np.argmin(c, axis=0)  # use unused rows of m
+            # print(row)
+            m[1:-1] = energy[row, :] + np.nanmin(c, axis=0)
+
+            # save the indexes for forward
+            keys[row, :] = \
+                idx + np.nanargmin(c, axis=0) - 1
+        best_idx = np.nanargmin(m)
+        # find the best seam save it to seams
+        corr_seam = find_best_seam(keys, best_idx, idx_mat, seams[:, i])
+        # delete the seam
+        image, idx_mat = delete_seam(image[:,1:-1],idx_mat,corr_seam)
+    return seams
 
 
 def resize(image: NDArray, out_height: int, out_width: int, forward_implementation: bool) -> Dict[str, NDArray]:
@@ -85,12 +101,14 @@ def resize(image: NDArray, out_height: int, out_width: int, forward_implementati
             where img1 is the resized image and img2/img3 are the visualization images
             (where the chosen seams are colored red and black for vertical and horizontal seams, respectively).
     """
+    def o_idx(idx: int) -> Tuple[int, int]:
+        return idx // o_width, idx % o_width
+
     o_height, o_width, _ = image.shape
     gray = utils.to_grayscale(image)
     idx_mat = np.arange(o_height * o_width).reshape(o_height, o_width)
+    find_vrt_seams(gray, 1, idx_mat)
 
-    def o_idx(idx: int) -> Tuple[int, int]:
-        return idx // o_width, idx % o_width
 
     vrt_k, hrz_k = abs(out_width - o_width), abs(out_height - o_height)
     vrt_seams = []
